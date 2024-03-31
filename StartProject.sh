d429807f28ce7833072e9adb1de904c84df71132
leo98ber <leonardoberoes94@gmail.com>
2024-03-31 11:31:07 -0400

    feat: add StartProject.sh file
    BREAKING CHANGE: This file generate a complete django api with the parameters that user enter in shell

---
+++ StartProject.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+source ./AddComposeFile.sh
+source django/AddDjangoService.sh
+source celery/AddCeleryService.sh
+
+echo "Ingrese la ubicación donde desea crear el proyecto:"
+read project_path
+echo -e "\n"
+
+cd "$project_path"
+
+#python3 -m venv venv
+
+source venv/bin/activate
+
+echo "Ingrese la versión de Django REST Framework que desea instalar:"
+read drf_version
+echo -e "\n"
+
+
+if [ -z "$drf_version" ]; then
+#  pip install djangorestframework --upgrade
+  drf_version=$(pip show djangorestframework | grep Version | awk '{print $2}')
+  django_version=$(pip show Django | grep Version | awk '{print $2}')
+else
+  pip install "djangorestframework==$drf_version"
+  django_version=$(pip show Django | grep Version | awk '{print $2}')
+fi
+
+read -p "Ingrese el nombre del proyecto (por defecto: my_project): " project_name
+project_name=${project_name:-my_project}
+echo -e "\n"
+
+# Validate project name
+if [[ ! "$project_name" =~ ^[a-zA-Z0-9_-]+$ ]]; then
+  echo "Error: El nombre del proyecto solo puede tener letras, numeros, guiones bajos, and guiones."
+  echo -e "\n"
+  exit 1
+fi
+
+create_django_compose_file
+touch api.env
+#create_django_service drf_version django_version
+
+#django-admin startproject "${project_name}"
+
+#mv "${project_name}"/manage.py .
+#mv "${project_name}"/"${project_name}"/* ./"${project_name}"
+#rm -r "${project_name}"/"${project_name}"
+#
+
+echo `pwd`
+
+
+read -p "Desea agregar celery? (y/N) " -r confirm
+echo -e "\n"
+
+if [[ $confirm =~ ^[Yy]$ ]]; then
+  create_celery_service project_name
+fi
+
+
+#deactivate
+
+
