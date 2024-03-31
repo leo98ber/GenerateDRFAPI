051a584c7e31519ed8e43d2c8027b8c93c0dd2c2
leo98ber <leonardoberoes94@gmail.com>
2024-03-31 11:41:30 -0400

    feat: add AddCeleryInSettings.sh file
    BREAKING CHANGE: This file add celery configuration in django settings

---
+++ celery/AddCeleryInSettings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+project_name=$1
+
+function add_celery_in_settings() {
+    echo "
+import os
+
+INSTALLED_APPS.append('${project_name}.celery_cfg.CeleryAppConfig')
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+CELERY_CONFIG_MODULE = '${project_name}.celery_cfg'
+  " >> ${project_name}/settings.py
+
+}
+
